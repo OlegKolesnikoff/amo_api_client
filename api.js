1c1593ff2b5b16400c71a423bcaafcf1dfe3b971
OlegKolesnikoff <kolecnikovoleg@gmail.com>
2025-04-23 17:20:41 +0300

    change settings

--- api.js
+++ api.js
@@ -8,27 +8,7 @@ const userApi = require('./services/users');
 const pipleneApi = require('./services/pipelines');
 const tagApi = require('./services/tags');
 
-const chatSettings = {
-  DOMAIN: 'https://amojo.amocrm.ru',
-  AMO_CHANEL_ID: '',
-  AMO_CHANEL_SECRET: '',
-  HOOK_API_VERSION: 'v2',
-  TITLE: '',
-};
-
-const clientSettings = {
-  CLIENT_SECRET: null,
-  CLIENT_ID: null,
-  AMO_INSTALL_URL: null,
-};
-
-Object.assign(chatApi, chatSettings);
-Object.assign(authApi, clientSettings);
-
-const settings = { ...chatSettings, ...clientSettings };
-
 module.exports = {
-  settings,
   chatApi,
   authApi,
   accountApi,
