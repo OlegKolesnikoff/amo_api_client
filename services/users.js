cf7f8d4be683ebbaf2d47e154fd53882c7613a35
OlegKolesnikoff <kolecnikovoleg@gmail.com>
2025-05-14 15:41:28 +0300

    add getUser

--- services/users.js
+++ services/users.js
@@ -15,4 +15,19 @@ module.exports = {
       return result?._embedded?.users?.map((user) => ({ id: user.id, name: user.name, email: user.email }));
     }
   },
+
+  async getUser(userId, auth) {
+    const url = `https://${auth.domain}/api/v4/users/${userId}`;
+    const params = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${auth.access_token}`,
+      },
+    };
+    const result = await client.fetcher(url, params, auth);
+
+    if (result) {
+      return { id: result.id, name: result.name, email: result.email };
+    }
+  }
 };
